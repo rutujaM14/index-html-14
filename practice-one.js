46a894bb4827b5268724178d58f1ac2827e0ae16
rutujaM14 <148721906+rutujaM14@users.noreply.github.com>
2023-10-23 11:12:46 -0400

    Add files via upload

---
+++ practice-one.js
@@ -0,0 +1,19 @@
+let exampleObject = {
+    id: 1,
+    name: "your name"
+}
+
+
+console.log(exampleObject);
+console.log("My name is: ",exampleObject.name);
+
+
+function doSomething(){
+    let functionVar = 10;
+    for(let i =0; i <10; i++){
+        console.log(i);
+    }
+}
+
+
+doSomething();
